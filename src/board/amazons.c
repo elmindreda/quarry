e076026a455acf1bee2feca0cdc4f36a1b1a4b06
doublep <doublep@3f990595-2723-0410-941d-8edc91ad4238>
2004-12-23 21:38:47 +0000

    * board/amazons.c (amazons_parse_move): Also parse moves in A1-B2/C3 and A1-B2xC3 formats (wish #1952, emarkus@cs.ualberta.ca.)
    git-svn-id: svn://svn.gna.org/quarry/trunk@426 3f990595-2723-0410-941d-8edc91ad4238

--- src/board/amazons.c
+++ src/board/amazons.c
@@ -37,25 +37,25 @@ static inline int  amazon_has_any_legal_move
  * play according to Amazons rules (and thus loses the game).
  */
 int
-amazons_adjust_color_to_play(const Board *board, BoardRuleSet rule_set,
-			     int color)
+amazons_adjust_color_to_play (const Board *board, BoardRuleSet rule_set,
+			      int color)
 {
   int pos;
   int have_other_color = 0;
 
-  UNUSED(rule_set);
+  UNUSED (rule_set);
 
-  for (pos = POSITION(0, 0); ON_GRID(board->grid, pos);
+  for (pos = POSITION (0, 0); ON_GRID (board->grid, pos);
        pos += (BOARD_MAX_WIDTH + 1) - board->width) {
-    for (; ON_GRID(board->grid, pos); pos++) {
+    for (; ON_GRID (board->grid, pos); pos++) {
       if (board->grid[pos] == color)
 	return color;
-      else if (board->grid[pos] == OTHER_COLOR(color))
+      else if (board->grid[pos] == OTHER_COLOR (color))
 	have_other_color = 1;
     }
   }
 
-  return have_other_color ? OTHER_COLOR(color) : EMPTY;
+  return have_other_color ? OTHER_COLOR (color) : EMPTY;
 }
 
 
@@ -63,18 +63,18 @@ amazons_adjust_color_to_play(const Board *board, BoardRuleSet rule_set,
  * for `color_to_play'.
  */
 int
-amazons_is_game_over(const Board *board, BoardRuleSet rule_set,
-		     int color_to_play)
+amazons_is_game_over (const Board *board, BoardRuleSet rule_set,
+		      int color_to_play)
 {
   int pos;
 
-  assert(rule_set < NUM_AMAZONS_RULE_SETS);
+  assert (rule_set < NUM_AMAZONS_RULE_SETS);
 
-  for (pos = POSITION(0, 0); ON_GRID(board->grid, pos);
+  for (pos = POSITION (0, 0); ON_GRID (board->grid, pos);
        pos += (BOARD_MAX_WIDTH + 1) - board->width) {
-    for (; ON_GRID(board->grid, pos); pos++) {
+    for (; ON_GRID (board->grid, pos); pos++) {
       if (board->grid[pos] == color_to_play
-	  && amazon_has_any_legal_move(board->grid, pos))
+	  && amazon_has_any_legal_move (board->grid, pos))
 	return 0;
     }
   }
@@ -85,26 +85,26 @@ amazons_is_game_over(const Board *board, BoardRuleSet rule_set,
 
 /* Get the default setup for Amazons. */
 int
-amazons_get_default_setup(int width, int height,
-			  BoardPositionList **black_stones,
-			  BoardPositionList **white_stones)
+amazons_get_default_setup (int width, int height,
+			   BoardPositionList **black_stones,
+			   BoardPositionList **white_stones)
 {
   int x1 = (width - 1) / 3;
   int x2 = (width - 1) - x1;
   int y1 = (height - 1) / 3;
   int y2 = (height - 1) - y1;
 
-  *black_stones = board_position_list_new_empty(4);
-  (*black_stones)->positions[0] = POSITION(x1, 0);
-  (*black_stones)->positions[1] = POSITION(x2, 0);
-  (*black_stones)->positions[2] = POSITION(0, y1);
-  (*black_stones)->positions[3] = POSITION(width - 1, y1);
+  *black_stones = board_position_list_new_empty (4);
+  (*black_stones)->positions[0] = POSITION (x1, 0);
+  (*black_stones)->positions[1] = POSITION (x2, 0);
+  (*black_stones)->positions[2] = POSITION (0, y1);
+  (*black_stones)->positions[3] = POSITION (width - 1, y1);
 
-  *white_stones = board_position_list_new_empty(4);
-  (*white_stones)->positions[0] = POSITION(0, y2);
-  (*white_stones)->positions[1] = POSITION(width - 1, y2);
-  (*white_stones)->positions[2] = POSITION(x1, height - 1);
-  (*white_stones)->positions[3] = POSITION(x2, height - 1);
+  *white_stones = board_position_list_new_empty (4);
+  (*white_stones)->positions[0] = POSITION (0, y2);
+  (*white_stones)->positions[1] = POSITION (width - 1, y2);
+  (*white_stones)->positions[2] = POSITION (x1, height - 1);
+  (*white_stones)->positions[3] = POSITION (x2, height - 1);
 
   return 1;
 }
@@ -117,8 +117,8 @@ amazons_get_default_setup(int width, int height,
  * given "from" and "to" points (there is always at least one).
  */
 int
-amazons_is_legal_move(const Board *board, BoardRuleSet rule_set,
-		      int color, va_list move)
+amazons_is_legal_move (const Board *board, BoardRuleSet rule_set,
+		       int color, va_list move)
 {
   const char *grid = board->grid;
   int pos;
@@ -130,50 +130,50 @@ amazons_is_legal_move(const Board *board, BoardRuleSet rule_set,
   int shoot_arrow_to_pos;
   int direction;
 
-  assert(rule_set < NUM_AMAZONS_RULE_SETS);
+  assert (rule_set < NUM_AMAZONS_RULE_SETS);
 
-  to_x	    = va_arg(move, int);
-  to_y	    = va_arg(move, int);
-  move_data = va_arg(move, BoardAmazonsMoveData);
+  to_x	    = va_arg (move, int);
+  to_y	    = va_arg (move, int);
+  move_data = va_arg (move, BoardAmazonsMoveData);
 
-  assert(ON_BOARD(board, move_data.from.x, move_data.from.y));
+  assert (ON_BOARD (board, move_data.from.x, move_data.from.y));
 
-  from_pos = POINT_TO_POSITION(move_data.from);
+  from_pos = POINT_TO_POSITION (move_data.from);
   if (grid[from_pos] != color)
     return 0;
 
   if (rule_set == AMAZONS_RULE_SET_SGF)
     return 1;
 
-  if (IS_NULL_POINT(to_x, to_y))
-    return amazon_has_any_legal_move(grid, from_pos);
+  if (IS_NULL_POINT (to_x, to_y))
+    return amazon_has_any_legal_move (grid, from_pos);
 
-  assert(ON_BOARD(board, to_x, to_y));
+  assert (ON_BOARD (board, to_x, to_y));
 
-  direction = determine_position_delta(to_x - move_data.from.x,
-				       to_y - move_data.from.y);
+  direction = determine_position_delta (to_x - move_data.from.x,
+					to_y - move_data.from.y);
   if (!direction)
     return 0;
 
-  to_pos = POSITION(to_x, to_y);
+  to_pos = POSITION (to_x, to_y);
   for (pos = from_pos; pos != to_pos; ) {
     pos += direction;
     if (grid[pos] != EMPTY)
       return 0;
   }
 
-  if (IS_NULL_POINT(move_data.shoot_arrow_to.x, move_data.shoot_arrow_to.y))
+  if (IS_NULL_POINT (move_data.shoot_arrow_to.x, move_data.shoot_arrow_to.y))
     return 1;
 
-  assert(ON_BOARD(board,
-		  move_data.shoot_arrow_to.x, move_data.shoot_arrow_to.y));
+  assert (ON_BOARD (board,
+		    move_data.shoot_arrow_to.x, move_data.shoot_arrow_to.y));
 
-  direction = determine_position_delta(move_data.shoot_arrow_to.x - to_x,
-				       move_data.shoot_arrow_to.y - to_y);
+  direction = determine_position_delta (move_data.shoot_arrow_to.x - to_x,
+					move_data.shoot_arrow_to.y - to_y);
   if (!direction)
     return 0;
 
-  shoot_arrow_to_pos = POINT_TO_POSITION(move_data.shoot_arrow_to);
+  shoot_arrow_to_pos = POINT_TO_POSITION (move_data.shoot_arrow_to);
   do {
     pos += direction;
     if (grid[pos] != EMPTY && pos != from_pos)
@@ -185,7 +185,7 @@ amazons_is_legal_move(const Board *board, BoardRuleSet rule_set,
 
 
 static inline int
-amazon_has_any_legal_move(const char grid[BOARD_FULL_GRID_SIZE], int pos)
+amazon_has_any_legal_move (const char grid[BOARD_FULL_GRID_SIZE], int pos)
 {
   int k;
 
@@ -199,30 +199,30 @@ amazon_has_any_legal_move(const char grid[BOARD_FULL_GRID_SIZE], int pos)
 
 
 void
-amazons_play_move(Board *board, int color, va_list move)
+amazons_play_move (Board *board, int color, va_list move)
 {
   char *grid = board->grid;
   int to_x;
   int to_y;
   AmazonsMoveStackEntry *stack_entry
-    = ALLOCATE_AMAZONS_MOVE_STACK_ENTRY(board);
+    = ALLOCATE_AMAZONS_MOVE_STACK_ENTRY (board);
   BoardAmazonsMoveData move_data;
 
-  to_x	    = va_arg(move, int);
-  to_y	    = va_arg(move, int);
-  move_data = va_arg(move, BoardAmazonsMoveData);
+  to_x	    = va_arg (move, int);
+  to_y	    = va_arg (move, int);
+  move_data = va_arg (move, BoardAmazonsMoveData);
 
-  assert(ON_BOARD(board, move_data.from.x, move_data.from.y));
-  assert(ON_BOARD(board, to_x, to_y));
-  assert(ON_BOARD(board,
-		  move_data.shoot_arrow_to.x, move_data.shoot_arrow_to.y));
+  assert (ON_BOARD (board, move_data.from.x, move_data.from.y));
+  assert (ON_BOARD (board, to_x, to_y));
+  assert (ON_BOARD (board,
+		    move_data.shoot_arrow_to.x, move_data.shoot_arrow_to.y));
 
-  assert(grid[POINT_TO_POSITION(move_data.from)] == color);
+  assert (grid[POINT_TO_POSITION (move_data.from)] == color);
 
-  stack_entry->from		   = POINT_TO_POSITION(move_data.from);
-  stack_entry->to		   = POSITION(to_x, to_y);
+  stack_entry->from		   = POINT_TO_POSITION (move_data.from);
+  stack_entry->to		   = POSITION (to_x, to_y);
   stack_entry->misc.shoot_arrow_to
-    = POINT_TO_POSITION(move_data.shoot_arrow_to);
+    = POINT_TO_POSITION (move_data.shoot_arrow_to);
 
   stack_entry->to_contents = grid[stack_entry->to];
   stack_entry->shoot_arrow_to_contents
@@ -237,9 +237,9 @@ amazons_play_move(Board *board, int color, va_list move)
 
 
 void
-amazons_undo(Board *board)
+amazons_undo (Board *board)
 {
-  AmazonsMoveStackEntry *stack_entry = POP_AMAZONS_MOVE_STACK_ENTRY(board);
+  AmazonsMoveStackEntry *stack_entry = POP_AMAZONS_MOVE_STACK_ENTRY (board);
 
   if (stack_entry->from != NULL_POSITION) {
     board->grid[stack_entry->from]	     = board->grid[stack_entry->to];
@@ -248,17 +248,17 @@ amazons_undo(Board *board)
       = stack_entry->shoot_arrow_to_contents;
   }
   else if (stack_entry->misc.num_changes > 0)
-    board_undo_changes(board, stack_entry->misc.num_changes);
+    board_undo_changes (board, stack_entry->misc.num_changes);
 
   board->move_number = stack_entry->move_number;
 }
 
 
 void
-amazons_apply_changes(Board *board, int num_changes)
+amazons_apply_changes (Board *board, int num_changes)
 {
   AmazonsMoveStackEntry *stack_entry
-    = ALLOCATE_AMAZONS_MOVE_STACK_ENTRY(board);
+    = ALLOCATE_AMAZONS_MOVE_STACK_ENTRY (board);
 
   stack_entry->from		= NULL_POSITION;
   stack_entry->misc.num_changes = num_changes;
@@ -267,21 +267,21 @@ amazons_apply_changes(Board *board, int num_changes)
 
 
 void
-amazons_add_dummy_move_entry(Board *board)
+amazons_add_dummy_move_entry (Board *board)
 {
-  amazons_apply_changes(board, 0);
+  amazons_apply_changes (board, 0);
 }
 
 
 void
-amazons_validate_board(const Board *board)
+amazons_validate_board (const Board *board)
 {
-  UNUSED(board);
+  UNUSED (board);
 }
 
 
 void
-amazons_dump_board(const Board *board)
+amazons_dump_board (const Board *board)
 {
   static const char contents[5] = {'.', '@', 'O', '=', '?'};
   static const char coordinates[] =
@@ -290,73 +290,81 @@ amazons_dump_board(const Board *board)
   int x;
   int y;
 
-  fprintf(stderr, "   %.*s\n", board->width * 2, coordinates);
+  fprintf (stderr, "   %.*s\n", board->width * 2, coordinates);
 
   for (y = 0; y < board->height; y++) {
-    fprintf(stderr, "%3d", board->height - y);
+    fprintf (stderr, "%3d", board->height - y);
 
     for (x = 0; x < board->width; x++)
-      fprintf(stderr, " %c", contents[(int) board->grid[POSITION(x, y)]]);
+      fprintf (stderr, " %c", contents[(int) board->grid[POSITION (x, y)]]);
 
-    fprintf(stderr, "%3d\n", board->height - y);
+    fprintf (stderr, "%3d\n", board->height - y);
   }
 
-  fprintf(stderr, "   %.*s\n", board->width * 2, coordinates);
+  fprintf (stderr, "   %.*s\n", board->width * 2, coordinates);
 }
 
 
 int
-amazons_format_move(int board_width, int board_height,
-		    char *buffer, va_list move)
+amazons_format_move (int board_width, int board_height,
+		     char *buffer, va_list move)
 {
-  int x = va_arg(move, int);
-  int y = va_arg(move, int);
-  BoardAmazonsMoveData move_data = va_arg(move, BoardAmazonsMoveData);
+  int x = va_arg (move, int);
+  int y = va_arg (move, int);
+  BoardAmazonsMoveData move_data = va_arg (move, BoardAmazonsMoveData);
   char *pointer = buffer;
 
-  pointer += game_format_point(GAME_AMAZONS, board_width, board_height,
-			       pointer, move_data.from.x, move_data.from.y);
+  pointer += game_format_point (GAME_AMAZONS, board_width, board_height,
+				pointer, move_data.from.x, move_data.from.y);
   *pointer++ = '-';
-  pointer += game_format_point(GAME_AMAZONS, board_width, board_height,
-			       pointer, x, y);
+  pointer += game_format_point (GAME_AMAZONS, board_width, board_height,
+				pointer, x, y);
   *pointer++ = '-';
-  pointer += game_format_point(GAME_AMAZONS, board_width, board_height,
-			       pointer,
-			       move_data.shoot_arrow_to.x,
-			       move_data.shoot_arrow_to.y);
+  pointer += game_format_point (GAME_AMAZONS, board_width, board_height,
+				pointer,
+				move_data.shoot_arrow_to.x,
+				move_data.shoot_arrow_to.y);
 
   return pointer - buffer;
 }
 
 
+/* Parse an Amazons move.  Quarry understands three commonly used (and
+ * similar to each other) formats: A1-B2-C3, A1-B2/C3 and A1-B2xC3.
+ * This is of course only half of the work, as when speaking GTP, the
+ * other side has to understand us too (we speak the first vartiant.)
+ */
 int
-amazons_parse_move(int board_width, int board_height, const char *move_string,
-		   int *x, int *y, BoardAbstractMoveData *move_data)
+amazons_parse_move (int board_width, int board_height, const char *move_string,
+		    int *x, int *y, BoardAbstractMoveData *move_data)
 {
   int x_temp;
   int y_temp;
   int num_characters_eaten;
 
-  assert(move_data);
+  assert (move_data);
 
-  num_characters_eaten = game_parse_point(GAME_AMAZONS,
-					  board_width, board_height,
-					  move_string, &x_temp, &y_temp);
+  num_characters_eaten = game_parse_point (GAME_AMAZONS,
+					   board_width, board_height,
+					   move_string, &x_temp, &y_temp);
   if (num_characters_eaten > 0 && move_string[num_characters_eaten] == '-') {
     const char *pointer = move_string + num_characters_eaten + 1;
 
     move_data->amazons.from.x = x_temp;
     move_data->amazons.from.y = y_temp;
 
-    num_characters_eaten = game_parse_point(GAME_AMAZONS,
-					    board_width, board_height,
-					    pointer, x, y);
-    if (num_characters_eaten > 0 && pointer[num_characters_eaten] == '-') {
+    num_characters_eaten = game_parse_point (GAME_AMAZONS,
+					     board_width, board_height,
+					     pointer, x, y);
+    if (num_characters_eaten > 0
+	&& (pointer[num_characters_eaten] == '-'
+	    || pointer[num_characters_eaten] == '/'
+	    || pointer[num_characters_eaten] == 'x')) {
       pointer += num_characters_eaten + 1;
 
-      num_characters_eaten = game_parse_point(GAME_AMAZONS,
-					      board_width, board_height,
-					      pointer, &x_temp, &y_temp);
+      num_characters_eaten = game_parse_point (GAME_AMAZONS,
+					       board_width, board_height,
+					       pointer, &x_temp, &y_temp);
       if (num_characters_eaten) {
 	move_data->amazons.shoot_arrow_to.x = x_temp;
 	move_data->amazons.shoot_arrow_to.y = y_temp;
