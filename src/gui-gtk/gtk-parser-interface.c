6a735fb06cef8057d8417314f13a5827d1a6d4a3
doublep <doublep@3f990595-2723-0410-941d-8edc91ad4238>
2004-12-29 21:29:10 +0000

    * gui-gtk/gtk-parser-interface.c (analyze_parsed_data): Cast `data->parent' to GtkWindow type only when non-NULL.  If there is no parent window, make error dialog non-modal and register it with gtk_control_center_window_created(). (gtk_parse_sgf_file): Likewise.  Also fix for changed gtk_utils_create_message_dialog() prototype.
    git-svn-id: svn://svn.gna.org/quarry/trunk@445 3f990595-2723-0410-941d-8edc91ad4238

--- src/gui-gtk/gtk-parser-interface.c
+++ src/gui-gtk/gtk-parser-interface.c
@@ -45,7 +45,7 @@
 /* For chdir() function. */
 #if HAVE_UNISTD_H
 #include <unistd.h>
-#elif defined(G_OS_WIN32)
+#elif defined (G_OS_WIN32)
 
 /* FIXME: verify that it works. */
 #include <windows.h>
@@ -67,36 +67,38 @@ static const gchar *not_sgf_file_error_hint =
      "make sure you select a proper SGF file.");
 
 
-static void	 open_file_response(GtkFileSelection *dialog, gint response_id);
+static void	 open_file_response (GtkFileSelection *dialog,
+				     gint response_id);
 
 
 void
-gtk_parser_interface_present(void)
+gtk_parser_interface_present (void)
 {
-  GtkWidget *file_selection = gtk_file_selection_new(_("Open SGF File..."));
+  GtkWidget *file_selection = gtk_file_selection_new (_("Open SGF File..."));
 
-  gtk_control_center_window_created(GTK_WINDOW(file_selection));
+  gtk_control_center_window_created (GTK_WINDOW (file_selection));
 
-  gtk_utils_add_file_selection_response_handlers(file_selection, FALSE,
-						 G_CALLBACK(open_file_response),
-						 NULL);
-  g_signal_connect(file_selection, "destroy",
-		   G_CALLBACK(gtk_control_center_window_destroyed), NULL);
+  gtk_utils_add_file_selection_response_handlers (file_selection, FALSE,
+						  (G_CALLBACK
+						   (open_file_response)),
+						  NULL);
+  g_signal_connect (file_selection, "destroy",
+		    G_CALLBACK (gtk_control_center_window_destroyed), NULL);
 
-  gtk_widget_show(file_selection);
+  gtk_widget_show (file_selection);
 }
 
 
 static void
-open_file_response(GtkFileSelection *dialog, gint response_id)
+open_file_response (GtkFileSelection *dialog, gint response_id)
 {
   if (response_id == GTK_RESPONSE_OK) {
-    const gchar *filename = gtk_file_selection_get_filename(dialog);
+    const gchar *filename = gtk_file_selection_get_filename (dialog);
 
-    gtk_parse_sgf_file(filename, GTK_WINDOW(dialog));
+    gtk_parse_sgf_file (filename, GTK_WINDOW (dialog));
   }
   else if (response_id == GTK_RESPONSE_CANCEL)
-    gtk_widget_destroy(GTK_WIDGET(dialog));
+    gtk_widget_destroy (GTK_WIDGET (dialog));
 }
 
 
@@ -106,82 +108,82 @@ open_file_response(GtkFileSelection *dialog, gint response_id)
 #include "gtk-progress-dialog.h"
 
 
-static gpointer	 thread_wrapped_sgf_parse_file(ParsingThreadData *data);
+static gpointer	 thread_wrapped_sgf_parse_file (ParsingThreadData *data);
 
-static gboolean	 update_progress_bar(GtkProgressDialog *progress_dialog,
-				     ParsingThreadData *data);
-static gboolean	 cancel_parsing(GtkProgressDialog *progress_dialog,
-				ParsingThreadData *data);
+static gboolean	 update_progress_bar (GtkProgressDialog *progress_dialog,
+				      ParsingThreadData *data);
+static gboolean	 cancel_parsing (GtkProgressDialog *progress_dialog,
+				 ParsingThreadData *data);
 
-static void	 analyze_parsed_data(void *result);
+static void	 analyze_parsed_data (void *result);
 
 
 void
-gtk_parse_sgf_file(const char *filename, GtkWindow *parent)
+gtk_parse_sgf_file (const char *filename, GtkWindow *parent)
 {
   ParsingThreadData *data;
   gchar *label_text;
 
-  data = g_malloc(sizeof(ParsingThreadData));
-  data->filename	  = g_strdup(filename);
+  data = g_malloc (sizeof (ParsingThreadData));
+  data->filename	  = g_strdup (filename);
   data->file_size	  = 0;
   data->bytes_parsed	  = 0;
   data->cancellation_flag = 0;
-  data->parent		  = (parent ? GTK_WIDGET(parent) : NULL);
+  data->parent		  = (parent ? GTK_WIDGET (parent) : NULL);
 
-  label_text = g_strdup_printf(_("Parsing file `%s'..."), filename);
+  label_text = g_strdup_printf (_("Parsing file `%s'..."), filename);
   data->progress_dialog
-    = gtk_progress_dialog_new(parent, "Quarry", label_text,
-			      (GtkProgressDialogCallback) update_progress_bar,
-			      (GtkProgressDialogCallback) cancel_parsing,
-			      data);
-  g_object_ref(data->progress_dialog);
-  g_free(label_text);
+    = gtk_progress_dialog_new (parent, "Quarry", label_text,
+			       (GtkProgressDialogCallback) update_progress_bar,
+			       (GtkProgressDialogCallback) cancel_parsing,
+			       data);
+  g_object_ref (data->progress_dialog);
+  g_free (label_text);
 
-  gtk_control_center_new_reason_to_live();
+  gtk_control_center_new_reason_to_live ();
 
-  g_thread_create((GThreadFunc) thread_wrapped_sgf_parse_file,
-		  data, FALSE, NULL);
+  g_thread_create ((GThreadFunc) thread_wrapped_sgf_parse_file,
+		   data, FALSE, NULL);
 }
 
 
 static gpointer
-thread_wrapped_sgf_parse_file(ParsingThreadData *data)
+thread_wrapped_sgf_parse_file (ParsingThreadData *data)
 {
   ThreadEventData *event_data;
 
-  data->result = sgf_parse_file(data->filename,
-				&data->sgf_collection, &data->error_list,
-				&sgf_parser_defaults,
-				&data->file_size, &data->bytes_parsed,
-				&data->cancellation_flag);
+  data->result = sgf_parse_file (data->filename,
+				 &data->sgf_collection, &data->error_list,
+				 &sgf_parser_defaults,
+				 &data->file_size, &data->bytes_parsed,
+				 &data->cancellation_flag);
 
-  event_data = g_malloc(sizeof(ThreadEventData));
+  event_data = g_malloc (sizeof (ThreadEventData));
   event_data->callback = analyze_parsed_data;
   event_data->result   = data;
 
-  g_async_queue_push(thread_events_queue, event_data);
-  g_main_context_wakeup(NULL);
+  g_async_queue_push (thread_events_queue, event_data);
+  g_main_context_wakeup (NULL);
 
   return NULL;
 }
 
 
 static gboolean
-update_progress_bar(GtkProgressDialog *progress_dialog,
-		    ParsingThreadData *data)
+update_progress_bar (GtkProgressDialog *progress_dialog,
+		     ParsingThreadData *data)
 {
-  if (!GTK_WIDGET_VISIBLE(progress_dialog)) {
-    gtk_widget_show(GTK_WIDGET(progress_dialog));
+  if (!GTK_WIDGET_VISIBLE (progress_dialog)) {
+    gtk_widget_show (GTK_WIDGET (progress_dialog));
     if (data->parent)
-      gtk_widget_hide(data->parent);
+      gtk_widget_hide (data->parent);
   }
 
   if (data->file_size > 0) {
-    gtk_progress_dialog_set_fraction(progress_dialog,
-				     (((gdouble) data->bytes_parsed)
-				      / data->file_size),
-				     "Quarry");
+    gtk_progress_dialog_set_fraction (progress_dialog,
+				      (((gdouble) data->bytes_parsed)
+				       / data->file_size),
+				      "Quarry");
   }
 
   return TRUE;
@@ -189,9 +191,9 @@ update_progress_bar(GtkProgressDialog *progress_dialog,
 
 
 static gboolean
-cancel_parsing(GtkProgressDialog *progress_dialog, ParsingThreadData *data)
+cancel_parsing (GtkProgressDialog *progress_dialog, ParsingThreadData *data)
 {
-  UNUSED(progress_dialog);
+  UNUSED (progress_dialog);
 
   data->cancellation_flag = 1;
   return TRUE;
@@ -199,59 +201,67 @@ cancel_parsing(GtkProgressDialog *progress_dialog, ParsingThreadData *data)
 
 
 static void
-analyze_parsed_data(void *result)
+analyze_parsed_data (void *result)
 {
   ParsingThreadData *data = (ParsingThreadData *) result;
 
-  gtk_widget_destroy(data->progress_dialog);
+  gtk_widget_destroy (data->progress_dialog);
 
   if (data->result == SGF_PARSED) {
-    GtkWidget *goban_window = gtk_goban_window_new(data->sgf_collection,
-						   data->filename);
+    GtkWidget *goban_window = gtk_goban_window_new (data->sgf_collection,
+						    data->filename);
 
-    gtk_widget_show(goban_window);
+    gtk_widget_show (goban_window);
 
     if (data->error_list)
-      string_list_delete(data->error_list);
+      string_list_delete (data->error_list);
 
     if (data->parent) {
-      gchar *directory = g_path_get_dirname(data->filename);
+      gchar *directory = g_path_get_dirname (data->filename);
 
-      chdir(directory);
-      g_free(directory);
+      chdir (directory);
+      g_free (directory);
 
-      gtk_widget_destroy(data->parent);
+      gtk_widget_destroy (data->parent);
     }
   }
   else if (data->result == SGF_PARSING_CANCELLED) {
     if (data->parent)
-      gtk_widget_destroy(data->parent);
+      gtk_widget_destroy (data->parent);
   }
   else {
     GtkProgressDialog *progress_dialog
-      = GTK_PROGRESS_DIALOG(data->progress_dialog);
+      = GTK_PROGRESS_DIALOG (data->progress_dialog);
+    GtkWidget *message_dialog;
 
-    gtk_progress_dialog_recover_parent(progress_dialog);
+    gtk_progress_dialog_recover_parent (progress_dialog);
 
-    gtk_utils_create_message_dialog(GTK_WINDOW(data->parent),
-				    GTK_STOCK_DIALOG_ERROR,
-				    (GTK_UTILS_BUTTONS_OK
-				     | GTK_UTILS_DESTROY_ON_RESPONSE),
-				    (data->result == SGF_ERROR_READING_FILE
-				     ? _(reading_error_hint)
-				     : _(not_sgf_file_error_hint)),
-				    (data->result == SGF_ERROR_READING_FILE
-				     ? _(reading_error)
-				     : _(not_sgf_file_error)),
-				    data->filename);
+    message_dialog
+      = (gtk_utils_create_message_dialog
+	 (data->parent ? GTK_WINDOW (data->parent) : NULL,
+	  GTK_STOCK_DIALOG_ERROR,
+	  (GTK_UTILS_BUTTONS_OK | GTK_UTILS_DESTROY_ON_RESPONSE
+	   | (data->parent ? 0 : GTK_UTILS_NON_MODAL_WINDOW)),
+	  (data->result == SGF_ERROR_READING_FILE
+	   ? _(reading_error_hint) : _(not_sgf_file_error_hint)),
+	  (data->result == SGF_ERROR_READING_FILE
+	   ? _(reading_error) : _(not_sgf_file_error)),
+	  data->filename));
+
+    if (!data->parent) {
+      gtk_control_center_window_created (GTK_WINDOW (message_dialog));
+      g_signal_connect (message_dialog, "destroy",
+			G_CALLBACK (gtk_control_center_window_destroyed),
+			NULL);
+    }
   }
 
-  g_object_unref(data->progress_dialog);
+  g_object_unref (data->progress_dialog);
 
-  g_free(data->filename);
-  g_free(data);
+  g_free (data->filename);
+  g_free (data);
 
-  gtk_control_center_lost_reason_to_live();
+  gtk_control_center_lost_reason_to_live ();
 }
 
 
@@ -259,44 +269,51 @@ analyze_parsed_data(void *result)
 
 
 void
-gtk_parse_sgf_file(const char *filename, GtkWindow *parent)
+gtk_parse_sgf_file (const char *filename, GtkWindow *parent)
 {
   SgfCollection *sgf_collection;
   SgfErrorList *error_list;
   int result;
 
-  result = sgf_parse_file(filename, &sgf_collection, &error_list,
-			  &sgf_parser_defaults, NULL, NULL, NULL);
+  result = sgf_parse_file (filename, &sgf_collection, &error_list,
+			   &sgf_parser_defaults, NULL, NULL, NULL);
 
   if (result == SGF_PARSED) {
-    GtkWidget *goban_window = gtk_goban_window_new(sgf_collection, filename);
+    GtkWidget *goban_window = gtk_goban_window_new (sgf_collection, filename);
 
-    gtk_widget_show(goban_window);
+    gtk_widget_show (goban_window);
 
     if (error_list)
-      string_list_delete(error_list);
+      string_list_delete (error_list);
 
     if (parent) {
-      gchar *directory = g_path_get_dirname(filename);
+      gchar *directory = g_path_get_dirname (filename);
 
-      chdir(directory);
-      g_free(directory);
+      chdir (directory);
+      g_free (directory);
 
-      gtk_widget_destroy(GTK_WIDGET(parent));
+      gtk_widget_destroy (GTK_WIDGET (parent));
     }
   }
   else {
-    gtk_utils_create_message_dialog(GTK_WINDOW(data->parent),
-				    GTK_STOCK_DIALOG_ERROR,
-				    GTK_UTILS_BUTTONS_OK,
-				    G_CALLBACK(gtk_widget_destroy),
-				    (result == SGF_ERROR_READING_FILE
-				     ? _(reading_error_hint)
-				     : _(not_sgf_file_error_hint)),
-				    (result == SGF_ERROR_READING_FILE
-				     ? _(reading_error)
-				     : _(not_sgf_file_error)),
-				    data->filename);
+    GtkWidget *message_dialog
+      = (gtk_utils_create_message_dialog
+	 (data->parent ? GTK_WINDOW (data->parent) : NULL,
+	  GTK_STOCK_DIALOG_ERROR,
+	  (GTK_UTILS_BUTTONS_OK | GTK_UTILS_DESTROY_ON_RESPONSE
+	   | (data->parent ? 0 : GTK_UTILS_NON_MODAL_WINDOW)),
+	  (data->result == SGF_ERROR_READING_FILE
+	   ? _(reading_error_hint) : _(not_sgf_file_error_hint)),
+	  (data->result == SGF_ERROR_READING_FILE
+	   ? _(reading_error) : _(not_sgf_file_error)),
+	  data->filename));
+
+    if (!data->parent) {
+      gtk_control_center_window_created (GTK_WINDOW (message_dialog));
+      g_signal_connect (message_dialog, "destroy",
+			G_CALLBACK (gtk_control_center_window_destroyed),
+			NULL);
+    }
   }
 }
 
