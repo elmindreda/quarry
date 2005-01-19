f6493637df07c04b731647dc6b6a301aa7547435
doublep <doublep@3f990595-2723-0410-941d-8edc91ad4238>
2005-01-19 21:53:18 +0000

    * gui-gtk/gui-back-end.c (initialize_main_loop): Read site configuration file if the user doesn't have any configuration yet.
    git-svn-id: svn://svn.gna.org/quarry/trunk@496 3f990595-2723-0410-941d-8edc91ad4238

--- src/gui-gtk/gui-back-end.c
+++ src/gui-gtk/gui-back-end.c
@@ -1,7 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
  * This file is part of Quarry.                                    *
  *                                                                 *
- * Copyright (C) 2003, 2004 Paul Pogonyshev.                       *
+ * Copyright (C) 2003, 2004, 2005 Paul Pogonyshev                  *
  *                                                                 *
  * This program is free software; you can redistribute it and/or   *
  * modify it under the terms of the GNU General Public License as  *
@@ -17,7 +17,7 @@
  * License along with this program; if not, write to the Free      *
  * Software Foundation, Inc., 59 Temple Place - Suite 330,         *
  * Boston, MA 02111-1307, USA.                                     *
-\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
 #include "gtk-configuration.h"
@@ -34,14 +34,15 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <gtk/gtk.h>
 
 
-static void	 initialize_main_loop(void);
+static void	 initialize_main_loop (void);
 
-static void	 run_main_loop(void);
-static gboolean	 run_cleanup_tasks(gpointer data);
+static void	 run_main_loop (void);
+static gboolean	 run_cleanup_tasks (gpointer data);
 
 
 const char     *user_real_name;
@@ -53,83 +54,83 @@ static GSList  *objects_to_finalize = NULL;
 
 
 int
-gui_back_end_init(int *argc, char **argv[])
+gui_back_end_init (int *argc, char **argv[])
 {
 #if THREADS_SUPPORTED
   /* gdk_threads_init() is not called because only one (main) thread
    * deals with GUI.
    */
-  g_thread_init(NULL);
+  g_thread_init (NULL);
 #endif
 
-  gtk_rc_add_default_file(PACKAGE_DATA_DIR "/gtkrc");
+  gtk_rc_add_default_file (PACKAGE_DATA_DIR "/gtkrc");
 
-  return gtk_init_check(argc, argv);
+  return gtk_init_check (argc, argv);
 }
 
 
 int
-gui_back_end_main_default(void)
+gui_back_end_main_default (void)
 {
-  initialize_main_loop();
+  initialize_main_loop ();
 
-  gtk_control_center_present();
+  gtk_control_center_present ();
 
-  run_main_loop();
+  run_main_loop ();
 
   return 0;
 }
 
 
 int
-gui_back_end_main_open_files(int num_files, char **filenames)
+gui_back_end_main_open_files (int num_files, char **filenames)
 {
   int k;
 
-  initialize_main_loop();
+  initialize_main_loop ();
 
   for (k = 0; k < num_files; k++)
-    gtk_parse_sgf_file(filenames[k], NULL);
+    gtk_parse_sgf_file (filenames[k], NULL);
 
-  run_main_loop();
+  run_main_loop ();
 
   return 0;
 }
 
 
 void
-gui_back_end_register_object_to_finalize(void *object)
+gui_back_end_register_object_to_finalize (void *object)
 {
-  assert(G_IS_OBJECT(object));
+  assert (G_IS_OBJECT (object));
 
-  objects_to_finalize = g_slist_prepend(objects_to_finalize, object);
+  objects_to_finalize = g_slist_prepend (objects_to_finalize, object);
 }
 
 
 
 void *
-gui_back_end_timer_restart(void *timer_object)
+gui_back_end_timer_restart (void *timer_object)
 {
   if (timer_object) {
-    g_timer_start((GTimer *) timer_object);
+    g_timer_start ((GTimer *) timer_object);
     return timer_object;
   }
 
-  return g_timer_new();
+  return g_timer_new ();
 }
 
 
 void
-gui_back_end_timer_delete(void *timer_object)
+gui_back_end_timer_delete (void *timer_object)
 {
-  g_timer_destroy((GTimer *) timer_object);
+  g_timer_destroy ((GTimer *) timer_object);
 }
 
 
 double
-gui_back_end_timer_get_seconds_elapsed(void *timer_object)
+gui_back_end_timer_get_seconds_elapsed (void *timer_object)
 {
-  return g_timer_elapsed((GTimer *) timer_object, NULL);
+  return g_timer_elapsed ((GTimer *) timer_object, NULL);
 }
 
 
@@ -137,12 +138,12 @@ gui_back_end_timer_get_seconds_elapsed(void *timer_object)
 
 
 
-static gboolean  thread_event_callback(gpointer data);
+static gboolean  thread_event_callback (gpointer data);
 
-static gboolean	 thread_events_prepare(GSource *source, gint *timeout);
-static gboolean	 thread_events_check(GSource *source);
-static gboolean	 thread_events_dispatch(GSource *source, GSourceFunc callback,
-					gpointer user_data);
+static gboolean	 thread_events_prepare (GSource *source, gint *timeout);
+static gboolean	 thread_events_check (GSource *source);
+static gboolean	 thread_events_dispatch (GSource *source, GSourceFunc callback,
+					 gpointer user_data);
 
 
 static GSourceFuncs thread_events_functions = {
@@ -158,46 +159,46 @@ GAsyncQueue	*thread_events_queue;
 
 
 static gboolean
-thread_event_callback(gpointer data)
+thread_event_callback (gpointer data)
 {
   ThreadEventData *thread_completion_data = (ThreadEventData *) data;
 
-  thread_completion_data->callback(thread_completion_data->result);
+  thread_completion_data->callback (thread_completion_data->result);
 
   return TRUE;
 }
 
 
 static gboolean
-thread_events_prepare(GSource *source, gint *timeout)
+thread_events_prepare (GSource *source, gint *timeout)
 {
-  UNUSED(source);
+  UNUSED (source);
 
   *timeout = -1;
-  return g_async_queue_length(thread_events_queue) > 0;
+  return g_async_queue_length (thread_events_queue) > 0;
 }
 
 
 static gboolean
-thread_events_check(GSource *source)
+thread_events_check (GSource *source)
 {
-  UNUSED(source);
+  UNUSED (source);
 
-  return g_async_queue_length(thread_events_queue) > 0;
+  return g_async_queue_length (thread_events_queue) > 0;
 }
 
 
 static gboolean
-thread_events_dispatch(GSource *source, GSourceFunc callback,
-		       gpointer user_data)
+thread_events_dispatch (GSource *source, GSourceFunc callback,
+			gpointer user_data)
 {
-  gpointer data = g_async_queue_pop(thread_events_queue);
-  gboolean result = callback(data);
+  gpointer data = g_async_queue_pop (thread_events_queue);
+  gboolean result = callback (data);
 
-  UNUSED(source);
-  UNUSED(user_data);  
+  UNUSED (source);
+  UNUSED (user_data);  
 
-  g_free(data);
+  g_free (data);
 
   return result;
 }
@@ -208,67 +209,90 @@ thread_events_dispatch(GSource *source, GSourceFunc callback,
 
 
 static void
-initialize_main_loop(void)
+initialize_main_loop (void)
 {
-#if THREADS_SUPPORTED
-  thread_events_queue = g_async_queue_new();
+  GtpEngineList site_configuration_engines;
 
-  thread_events = g_source_new(&thread_events_functions, sizeof(GSource));
-  g_source_set_callback(thread_events, thread_event_callback, NULL, NULL);
-  g_source_attach(thread_events, NULL);
+#if THREADS_SUPPORTED
+  thread_events_queue = g_async_queue_new ();
+
+  thread_events = g_source_new (&thread_events_functions, sizeof (GSource));
+  g_source_set_callback (thread_events, thread_event_callback, NULL, NULL);
+  g_source_attach (thread_events, NULL);
 #endif
 
-  quarry_stock_init();
+  quarry_stock_init ();
 
-  user_real_name = g_get_real_name();
-  configuration_init(gtk_configuration_sections,
-		     NUM_GTK_CONFIGURATION_SECTIONS);
+  user_real_name = g_get_real_name ();
+  configuration_init (gtk_configuration_sections,
+		      NUM_GTK_CONFIGURATION_SECTIONS);
 
-  configuration_file = g_build_path(G_DIR_SEPARATOR_S,
-				    g_get_home_dir(), ".quarry", NULL);
-  configuration_read_from_file(gtk_configuration_sections,
-			       NUM_GTK_CONFIGURATION_SECTIONS,
-			       configuration_file);
+  /* Try to read site configuration file.  Later,
+   * configuration_write_to_file() will effectively copy all
+   * site-default settings to user's configuration, with any changes
+   * she has made.
+   *
+   * Site configuration file is meant for distributions, to
+   * e.g. automatically register GNU Go with Quarry.
+   */
+  configuration_read_from_file (gtk_configuration_sections,
+				NUM_GTK_CONFIGURATION_SECTIONS,
+				PACKAGE_DATA_DIR "/quarry.cfg");
 
-  gtk_preferences_init();
+  gtp_engine_list_init (&site_configuration_engines);
+  string_list_steal_items (&site_configuration_engines, &gtp_engines);
+
+  configuration_file = g_build_path (G_DIR_SEPARATOR_S,
+				     g_get_home_dir (), ".quarry", NULL);
+  configuration_read_from_file (gtk_configuration_sections,
+				NUM_GTK_CONFIGURATION_SECTIONS,
+				configuration_file);
+
+  configuration_combine_string_lists (&gtp_engines,
+				      &site_configuration_engines,
+				      (G_STRUCT_OFFSET
+				       (GtpEngineListItem,
+					site_configuration_name)));
+
+  gtk_preferences_init ();
 }
 
 
 static void
-run_main_loop(void)
+run_main_loop (void)
 {
-  g_timeout_add_full(G_PRIORITY_LOW, 10000, run_cleanup_tasks, NULL, NULL);
+  g_timeout_add_full (G_PRIORITY_LOW, 10000, run_cleanup_tasks, NULL, NULL);
 
-  gtk_main();
+  gtk_main ();
 
-  object_cache_free(&gtk_main_tile_set_cache);
-  object_cache_free(&gtk_sgf_markup_tile_set_cache);
+  object_cache_free (&gtk_main_tile_set_cache);
+  object_cache_free (&gtk_sgf_markup_tile_set_cache);
 
 #if THREADS_SUPPORTED
-  g_source_destroy(thread_events);
-  g_async_queue_unref(thread_events_queue);
+  g_source_destroy (thread_events);
+  g_async_queue_unref (thread_events_queue);
 #endif
 
-  g_slist_foreach(objects_to_finalize, (GFunc) g_object_unref, NULL);
-  g_slist_free(objects_to_finalize);
+  g_slist_foreach (objects_to_finalize, (GFunc) g_object_unref, NULL);
+  g_slist_free (objects_to_finalize);
 
-  configuration_write_to_file(gtk_configuration_sections,
-			      NUM_GTK_CONFIGURATION_SECTIONS,
-			      configuration_file);
-  g_free(configuration_file);
+  configuration_write_to_file (gtk_configuration_sections,
+			       NUM_GTK_CONFIGURATION_SECTIONS,
+			       configuration_file);
+  g_free (configuration_file);
 
-  configuration_dispose(gtk_configuration_sections,
-			NUM_GTK_CONFIGURATION_SECTIONS);
+  configuration_dispose (gtk_configuration_sections,
+			 NUM_GTK_CONFIGURATION_SECTIONS);
 }
 
 
 static gboolean
-run_cleanup_tasks(gpointer data)
+run_cleanup_tasks (gpointer data)
 {
-  UNUSED(data);
+  UNUSED (data);
 
-  object_cache_recycle_dump(&gtk_main_tile_set_cache, 0);
-  object_cache_recycle_dump(&gtk_sgf_markup_tile_set_cache, 0);
+  object_cache_recycle_dump (&gtk_main_tile_set_cache, 0);
+  object_cache_recycle_dump (&gtk_sgf_markup_tile_set_cache, 0);
 
   return TRUE;
 }
